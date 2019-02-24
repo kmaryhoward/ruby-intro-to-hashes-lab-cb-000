1b135e136dc8846e5a6ef18a4287357a3ebc9d37
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-24 21:05:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,14 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {}
-  monopoly[:railroads] = Hash.new
-  monopoly[:railroads][:pieces] = 4
-	monopoly[:railroads][:rent_in_dollars] = Hash.new
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {}
+			
+		}
+	}
 end
 
 def monopoly_with_third_tier
