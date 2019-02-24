44330f7e400ec849860c86ec5d32aaa6870acfde
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-24 21:10:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,21 +46,31 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {}
-  monopoly[:railroads] = Hash.new
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:rent_in_dollars] = Hash.new
-  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned]= 50
-  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
-  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
-  monopoly[:names] = Hash.new
-  monopoly[:names][:reading_railroad] = Hash.new
-  monopoly[:names][:pennsylvania_railroad] = Hash.new
-  monopoly[:names][:b_and_o_railroad] = Hash.new
-  monopoly[:names][:shortline] = Hash.new
-  monopoly[:names][:reading_railroad]["mortgage_value"] = 100
-  monopoly[:names][:pennsylvania_railroad]["mortgage_value"] = 200
-  monopoly[:names][:b_and_o_railroad]["mortgage_value"] = 400
-  monopoly[:names][:shortline]["mortgage_value"] = 800
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => {
+				:reading_railroad => {
+					"mortgage_value" => "$100"
+					
+				},
+				:pennsylvania_railroad => {
+					"mortgage_value" => "$200"
+				},
+				:b_and_o_railroad => {
+					"mortgage_value" => "$400"
+				},
+				:shortline => {
+					"mortgage_value" => "$800"
+				}
+			},
+			:rent_in_dollars => {
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
+			}
+
+		}
+	}
 end
