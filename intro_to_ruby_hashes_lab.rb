799c6670ccd29e39085286a4fbb0b9c01a753c83
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-24 20:56:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,14 @@ def monopoly_with_third_tier
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:rent_in_dollars] = Hash.new
   monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned]= 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  monopoly[:names] = Hash.new
+  monopoly[:names][:reading_railroad] = Hash.new
+  monopoly[:names][:pennsylvania_railroad] = Hash.new
+  monopoly[:names][:b_and_o_railroad] = Hash.new
+  monopoly[:names][:shortline] = Hash.new
 end
 
 def monopoly_with_fourth_tier
