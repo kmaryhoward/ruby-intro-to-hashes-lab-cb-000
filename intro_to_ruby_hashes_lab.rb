fa2e94768f305c8a0fc9f17146d5fdc801c5fa13
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-24 20:59:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,6 +35,21 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	monopoly = {}
+  monopoly[:railroads] = Hash.new
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:rent_in_dollars] = Hash.new
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned]= 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  monopoly[:names] = Hash.new
+  monopoly[:names][:reading_railroad] = Hash.new
+  monopoly[:names][:pennsylvania_railroad] = Hash.new
+  monopoly[:names][:b_and_o_railroad] = Hash.new
+  monopoly[:names][:shortline] = Hash.new
+  monopoly[:names][:reading_railroad]["mortgage_value"] = 100
+  monopoly[:names][:pennsylvania_railroad]["mortgage_value"] = 200
+  monopoly[:names][:b_and_o_railroad]["mortgage_value"] = 400
+  monopoly[:names][:shortline]["mortgage_value"] = 800
 end
